d6a19ff5f442324a3d56f4fef42a5254a0cbdec7
Taha Ansari <taha.m.ansari@gmail.com>
2024-07-28 11:32:28 +0530

    wip

--- components/Comments.js
+++ components/Comments.js
@@ -1,6 +1,21 @@
 // THIS SHOULD BE SERVER COMPONENT
 
-// const Comments = async () => {
+async function getComments() {
+  const fetchComments = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");
+  return fetchComments.json();
+}
+
+export default async function Comments() {
+  const comments = await getComments();
+  console.log(comments);
+  return (
+    <>
+      <h2>This is server comments component</h2>
+    </>
+  );
+}
+
+// async function Comments(){
 //     const fetchComments = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");
 //     const fetchCommentsData = await fetchComments.json();
 //     console.log(fetchCommentsData)
@@ -10,16 +25,3 @@
 //         </>
 //     )
 // }
-
-async function Comments(){
-    const fetchComments = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");
-    const fetchCommentsData = await fetchComments.json();
-    console.log(fetchCommentsData)
-    return(
-        <>
-            <h2>This is server comments component</h2>
-        </>
-    )
-}
-
-export default Comments;
